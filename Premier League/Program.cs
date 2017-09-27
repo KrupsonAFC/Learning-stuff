2fe9ef91e002f93a541fbb8108002bade73239e1
Krupson\Adrian <adrian.krupa@o2.pl>
2017-09-27 12:08:36 +0200

    Premier League Tabel - Step 3, 4 in progress

--- Premier League/Program.cs
+++ Premier League/Program.cs
@@ -10,59 +10,28 @@ namespace Premier_League
     {
         static void Main(string[] args)
         {
-            DisplayMenu(); 
-            bool done = false; // ciekawostka, bool z urzędu jest false, więc to '=false' niepotrzebne. ale lepiej dac jak nie dac
-            ConsoleKeyInfo number; // tu mnie zakoczyles, dlaczego jakas struktura? a nie po prostu 'char number'?
-            do
-            {
-                number = Console.ReadKey();
-                switch (number.KeyChar)
-                {
-                    case '1':
-                        Console.Clear();
-                        done = true;
-                        Console.WriteLine("Tu bedzie tabela");
-                        break;
-                    case '2':
-                        Console.Clear(); 
-                        done = true;
-                        //Console.WriteLine("Tu se bedziesz edytował liste teamow");
-                        EditTeamList();
-                        break;
-                    case '3':
-                        Console.Clear();
-                        done = true;
-                        Console.WriteLine("Tu bedziesz dodawał wyniki");
-                        break;
-                    case '4':
-                        Console.Clear();
-                        done = true;
-                        Console.WriteLine("Tu bedziesz zmieniał wyniki jak cos zjebałes");
-                        break;
-                    default:
-                        Console.WriteLine("\nPodaj poprawny numer kalaputniaku");
-                        break;
-
-                }
-            }
-            while (!done);
-            Console.ReadLine();
+            DisplayMenu();
+            MakeChoice();
         }
 
-        
         public static void DisplayMenu()
-        {
-            Console.WriteLine("1. Wyświetl tabelę\n" +
-                "2. Edytuj listę drużyn\n" +
-                "3. Dodaj rezultat spotkania\n" +
-                "4. Edytuj rezultat spotkania\n");
+        {            
+            List<string> Menu = new List<string>();
+            Menu.Add("1. Wyświetl tabelę");
+            Menu.Add("2. Edytuj listę drużyn");
+            Menu.Add("3. Dodaj rezultat spotkania");
+            Menu.Add("4. Edytuj rezultat spotkania");
+            Menu.Add("0. Zamknij program");
+
+            for (int i = 0; i < Menu.Count; i++)
+            {
+                Console.WriteLine(Menu[i]);
+            }
         }
 
         public static void EditTeamList()
         {
-            Console.WriteLine("1. Dodaj zespół\n" +
-                "2. Edytuj zespół\n" +
-                "3. Usuń zespół\n");
+            DisplayEditionMenu();
             bool check = false; //jak na gorze
             ConsoleKeyInfo number; // jak na gorze
             do
@@ -85,13 +54,74 @@ namespace Premier_League
                         check = true;
                         Console.WriteLine("Usuwanie zespołu");
                         break;
+                    case '0':
+                        Console.Clear();
+                        DisplayMenu();
+                        MakeChoice();
+                        break;
                     default:
                         Console.WriteLine("\nPodaj poprawny numer kalaputniaku");
                         break;
                 }
             }
             while (!check);
-            Console.ReadLine();
         }
+        
+        public static void DisplayEditionMenu()
+        {
+            List<string> Menu = new List<string>();
+            Menu.Add("1. Dodaj zespół");
+            Menu.Add("2. Edytuj zespół");
+            Menu.Add("3. Usuń zespół");
+            Menu.Add("0. Wróc do poprzedniego menu");
+            for (int i = 0; i < Menu.Count; i++)
+            {
+                Console.WriteLine(Menu[i]);
+            }
+        }
+
+        public static void MakeChoice()
+        {
+            bool check = false;
+            ConsoleKeyInfo choice;
+            do
+            {
+                choice = Console.ReadKey();
+                switch (choice.KeyChar)
+                {
+                    case '1':
+                        Console.Clear();
+                        check = true;
+                        Console.WriteLine("Tu bedzie tabela");
+                        break;
+                    case '2':
+                        Console.Clear();
+                        check = true;
+                        //Console.WriteLine("Tu se bedziesz edytował liste teamow");
+                        EditTeamList();
+                        break;
+                    case '3':
+                        Console.Clear();
+                        check = true;
+                        Console.WriteLine("Tu bedziesz dodawał wyniki");
+                        break;
+                    case '4':
+                        Console.Clear();
+                        check = true;
+                        Console.WriteLine("Tu bedziesz zmieniał wyniki jak cos zjebałes");
+                        break;
+                    case '0':
+
+                        break;
+                    default:
+                        Console.WriteLine("\nPodaj poprawny numer kalaputniaku");
+                        break;
+
+                }
+            }
+            while (!check);
+
+        } 
+        
     }
 }
---
+++ Premier League/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premier_League
+{
+    class Team
+    {
+        public string Name { get; set; }
+        public int position { get; set; }
+        public int points { get; set; }
+    }
+}
